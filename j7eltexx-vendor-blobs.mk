37165eb82df40f1d69014bafd1dfd505df66c3fb
muralivijay <muralidharan9845@gmail.com>
2022-06-06 19:06:49 +0530

    j7elexx: Update ril blobs from j700T NN.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- j7eltexx-vendor-blobs.mk
+++ j7eltexx-vendor-blobs.mk
@@ -144,6 +144,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     $(LOCAL_PATH)/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
     $(LOCAL_PATH)/proprietary/lib/libsec-ril-dsds.so:system/lib/libsec-ril-dsds.so \
+    $(LOCAL_PATH)/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     $(LOCAL_PATH)/proprietary/lib/hw/sensors.universal7580.so:system/lib/hw/sensors.universal7580.so \
     $(LOCAL_PATH)/proprietary/lib/libstagefright_hdcp.so:system/lib/libstagefright_hdcp.so \
     $(LOCAL_PATH)/proprietary/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
